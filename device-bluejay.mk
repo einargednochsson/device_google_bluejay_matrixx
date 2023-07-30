8afe1ccc8a5901038922d827d21d2e98350a8de2
Einar Gednochsson <pete80pro.pb@gmail.com>
2023-07-30 04:46:37 +0000

    bluejay: add NowPlaying Overlay

--- device-bluejay.mk
+++ device-bluejay.mk
@@ -223,6 +223,10 @@ PRODUCT_VENDOR_PROPERTIES += \
 PRODUCT_PACKAGES += \
     NoCutoutOverlay \
     AvoidAppsInCutoutOverlay
+ 
+# NowPlayingOverlay
+PRODUCT_PACKAGES += \
+        NowPlayingOverlay
 
 # SKU specific RROs
 PRODUCT_PACKAGES += \
